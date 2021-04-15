3f347b79236203f6e136cc32efceae1a14e44b2f
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-15 12:11:35 -0500

    commit tree 00873475cc9955cf38d0dba8e306944f2ad7e4ef

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,63 @@
 // Code YouTubeDebugger Component Here
+// Code DigitalClicker Component Here
+
+import React from "react";
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: "1080p",
+        },
+      },
+    };
+  }
+
+  handleBitrateClick = () => {
+    this.setState((prevState) => {
+      console.log(prevState);
+      return {
+        settings: {
+          ...this.state.settings,
+          bitrate: 12,
+        },
+      };
+    });
+  };
+
+  handleResolutionClick = () => {
+    this.setState((prevState) => {
+      console.log(prevState);
+      return {
+        settings: {
+          ...this.state.settings,
+          video: {
+            ...this.state.settings.video,
+            resolution: "720p",
+          },
+        },
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        YouTubeDebugger
+        <button onClick={this.handleBitrateClick} className={"bitrate"}>
+          Bitrate
+        </button>
+        <button onClick={this.handleResolutionClick} className={"resolution"}>
+          Resolution
+        </button>
+      </div>
+    );
+  }
+}
+
+export default YouTubeDebugger;
